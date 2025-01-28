93906a7577386d99bdfe63b44c00433cc021d5bc
Dmytro Tytarenko <dtytarenko@protonmail.com>
2025-01-28 23:56:06 +0200

    fix gitignore for init project [SCRUM-1]

---
+++ wp-content/plugins/wc-ukr-shipping-pro/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bc6ebd01a2766dd1bde8d71fc974ac6eeeeb0abe',
+        'name' => 'kirillbdev/wc-ukr-shipping-pro',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'kirillbdev/wc-ukr-shipping-pro' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bc6ebd01a2766dd1bde8d71fc974ac6eeeeb0abe',
+            'dev_requirement' => false,
+        ),
+        'kirillbdev/wcus-core' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../kirillbdev/wcus-core',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '6729dd223adc31c3002d1a0b15083efdf7490c44',
+            'dev_requirement' => false,
+        ),
+    ),
+);
