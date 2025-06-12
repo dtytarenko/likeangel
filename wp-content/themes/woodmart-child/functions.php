f3a12a6765da4924c2f37e386bd731a2ba091697
Dmitry Tytarenko <38558456+dtytarenko@users.noreply.github.com>
2025-06-12 21:54:22 +0300

    Remove duplicate filter line

--- wp-content/themes/woodmart-child/functions.php
+++ wp-content/themes/woodmart-child/functions.php
@@ -120,7 +120,6 @@ function likeangel_enqueue_checkout_guard() {
 add_action( 'wp_enqueue_scripts', 'likeangel_enqueue_checkout_guard' );
 
 // Дозволяємо показувати всі варіації незалежно від "Показати варіант продукту"
-add_filter( 'woocommerce_hide_invisible_variations', '__return_false' );
 
 // Фікс: робимо варіацію доступною до покупки навіть якщо вона без запасу, але дозволене резервування
 add_filter( 'woocommerce_variation_is_purchasable', function( $purchasable, $variation ) {
