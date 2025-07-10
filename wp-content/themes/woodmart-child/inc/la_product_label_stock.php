6fe65323a730f24b4917c88a5ddb19f6603df81c
Dmytro Tytarenko <dtytarenko@protonmail.com>
2025-07-11 00:01:48 +0300

    [SCRUM-37] Fix out-of-stock badge visibility for variable products with available or backorderable variations

--- wp-content/themes/woodmart-child/inc/la_product_label_stock.php
+++ wp-content/themes/woodmart-child/inc/la_product_label_stock.php
@@ -5,7 +5,7 @@
  * @package Woodmart_Child
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
@@ -17,35 +17,35 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @param array $labels Array of label HTML strings.
  * @return array
  */
-function la_hide_out_of_stock_label( $labels ) {
+function la_hide_out_of_stock_label($labels)
+{
     global $product;
 
-    if ( ! $product instanceof WC_Product ) {
+    if (!$product instanceof WC_Product) {
         return $labels;
     }
 
     $hide_label = false;
 
-    if ( $product->is_type( 'variable' ) ) {
-        foreach ( $product->get_children() as $variation_id ) {
-            $variation = wc_get_product( $variation_id );
-            if ( ! $variation ) {
+    if ($product->is_type('variable')) {
+        foreach ($product->get_children() as $variation_id) {
+            $variation = wc_get_product($variation_id);
+            if (!$variation) {
                 continue;
             }
 
-            if ( $variation->is_in_stock() || $variation->backorders_allowed() ) {
+            if ($variation->is_in_stock() || $variation->get_backorders() !== 'no') {
                 $hide_label = true;
                 break;
             }
         }
-    } elseif ( $product->is_type( 'simple' ) && $product->backorders_allowed() ) {
-        $hide_label = true;
     }
 
-    if ( $hide_label ) {
-        foreach ( $labels as $index => $label_html ) {
-            if ( false !== strpos( $label_html, 'out-of-stock product-label' ) ) {
-                unset( $labels[ $index ] );
+
+    if ($hide_label) {
+        foreach ($labels as $index => $label_html) {
+            if (false !== strpos($label_html, 'out-of-stock product-label')) {
+                unset($labels[$index]);
             }
         }
     }
@@ -53,4 +53,4 @@ function la_hide_out_of_stock_label( $labels ) {
     return $labels;
 }
 
-add_filter( 'woodmart_product_label_output', 'la_hide_out_of_stock_label', 10, 1 );
+add_filter('woodmart_product_label_output', 'la_hide_out_of_stock_label', 10, 1);
